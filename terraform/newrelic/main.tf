a07b7f0774b67cacaf91da23d133e2e73a5cfb63
Tocyuki <tocguitar1@gmail.com>
2023-04-09 12:13:31 +0900

    create aws integration

---
+++ terraform/newrelic/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~>1.4.2"
+
+  required_providers {
+    aws = {
+      version = "~>4.59"
+      source  = "hashicorp/aws"
+    }
+
+    newrelic = {
+      source  = "newrelic/newrelic"
+      version = "~>3.20"
+    }
+  }
+}
+
+provider "newrelic" {
+  account_id = var.new_relic_account_id
+  region     = "US"
+  api_key    = var.new_relic_api_key
+}
---
+++ terraform/newrelic/variables.tf
@@ -0,0 +1,6 @@
+variable "new_relic_api_key" {}
+variable "new_relic_account_id" {}
+
+locals {
+  name = "terraform-newrelic-sample"
+}
