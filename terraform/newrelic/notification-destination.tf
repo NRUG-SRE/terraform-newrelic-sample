56fc31d1abb5d744ee3d27d01bda7816650e12b9
Tocyuki <tocguitar1@gmail.com>
2023-05-08 17:05:21 +0900

    delete comment out

--- terraform/newrelic/notification-destination.tf
+++ terraform/newrelic/notification-destination.tf
@@ -1,23 +1,23 @@
 variable "slack_workspace_name" {}
 variable "slack_destination_id" {}
 
-#resource "newrelic_notification_destination" "slack" {
-#  name = var.slack_workspace_name
-#  type = "SLACK"
-#
-#  lifecycle {
-#    ignore_changes = [auth_token]
-#  }
-#
-#  property {
-#    key   = "scope"
-#    label = "Permissions"
-#    value = "app_mentions:read,channels:join,channels:read,chat:write,chat:write.public,commands,groups:read,links:read,links:write,team:read,users:read"
-#  }
-#
-#  property {
-#    key   = "teamName"
-#    label = "Team Name"
-#    value = var.slack_workspace_name
-#  }
-#}
+resource "newrelic_notification_destination" "slack" {
+  name = var.slack_workspace_name
+  type = "SLACK"
+
+  lifecycle {
+    ignore_changes = [auth_token]
+  }
+
+  property {
+    key   = "scope"
+    label = "Permissions"
+    value = "app_mentions:read,channels:join,channels:read,chat:write,chat:write.public,commands,groups:read,links:read,links:write,team:read,users:read"
+  }
+
+  property {
+    key   = "teamName"
+    label = "Team Name"
+    value = var.slack_workspace_name
+  }
+}
--- terraform/newrelic/workflows.tf
+++ terraform/newrelic/workflows.tf
@@ -1,46 +1,46 @@
 variable "slack_channel_name" {}
 variable "slack_channel_id" {}
 
-#resource "newrelic_notification_channel" "slack" {
-#  account_id     = var.new_relic_account_id
-#  name           = var.slack_channel_name
-#  type           = "SLACK"
-#  destination_id = var.slack_destination_id
-#  product        = "IINT"
-#
-#  property {
-#    key   = "channelId"
-#    value = var.slack_channel_id
-#  }
-#
-#  property {
-#    key   = "customDetailsSlack"
-#    value = "issue id - {{issueId}}"
-#  }
-#}
-#
-#resource "newrelic_workflow" "slack" {
-#  name                  = "Slack Workflow"
-#  muting_rules_handling = "NOTIFY_ALL_ISSUES"
-#
-#  issues_filter {
-#    name = "Filter-name"
-#    type = "FILTER"
-#
-#    predicate {
-#      attribute = "labels.policyIds"
-#      operator  = "EXACTLY_MATCHES"
-#      values    = [newrelic_alert_policy.this.id]
-#    }
-#
-#    predicate {
-#      attribute = "priority"
-#      operator  = "EQUAL"
-#      values    = ["CRITICAL", "HIGH"]
-#    }
-#  }
-#
-#  destination {
-#    channel_id = newrelic_notification_channel.slack.id
-#  }
-#}
+resource "newrelic_notification_channel" "slack" {
+  account_id     = var.new_relic_account_id
+  name           = var.slack_channel_name
+  type           = "SLACK"
+  destination_id = var.slack_destination_id
+  product        = "IINT"
+
+  property {
+    key   = "channelId"
+    value = var.slack_channel_id
+  }
+
+  property {
+    key   = "customDetailsSlack"
+    value = "issue id - {{issueId}}"
+  }
+}
+
+resource "newrelic_workflow" "slack" {
+  name                  = "Slack Workflow"
+  muting_rules_handling = "NOTIFY_ALL_ISSUES"
+
+  issues_filter {
+    name = "Filter-name"
+    type = "FILTER"
+
+    predicate {
+      attribute = "labels.policyIds"
+      operator  = "EXACTLY_MATCHES"
+      values    = [newrelic_alert_policy.this.id]
+    }
+
+    predicate {
+      attribute = "priority"
+      operator  = "EQUAL"
+      values    = ["CRITICAL", "HIGH"]
+    }
+  }
+
+  destination {
+    channel_id = newrelic_notification_channel.slack.id
+  }
+}
